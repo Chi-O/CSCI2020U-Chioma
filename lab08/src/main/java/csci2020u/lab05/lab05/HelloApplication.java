3de6d3012438497bdabc72811808e8f0708fee71
Chi-O <chioma.o3762@gmail.com>
2022-03-29 12:45:26 -0400

    completed lab 08

--- lab08/src/main/java/csci2020u/lab05/lab05/HelloApplication.java
+++ lab08/src/main/java/csci2020u/lab05/lab05/HelloApplication.java
@@ -1,11 +1,16 @@
 package csci2020u.lab05.lab05;
 
+import com.opencsv.exceptions.CsvValidationException;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import com.opencsv.*;
 import java.io.*;
@@ -20,23 +25,34 @@ public class HelloApplication extends Application {
     static String currentFilename;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, CsvValidationException {
+        // get student records table view
+        // get student marks
+        ObservableList<StudentRecord> marks = getAllMarks();
+        TableView<StudentRecord> tvStudentRecords = getStudentRecords(marks);
+
+
         // create File menu instance
         Menu fileMenu = new Menu("File");
 
         // items to File menu
-        fileMenu.getItems().add(new MenuItem("New"));
-        fileMenu.getItems().add(new MenuItem("Open"));
-        fileMenu.getItems().add(new MenuItem("Save"));
-        fileMenu.getItems().add(new MenuItem("Save As"));
-        fileMenu.getItems().add(new MenuItem("Exit"));
+        MenuItem fileNew = new MenuItem("New");
+        MenuItem fileOpen = new MenuItem("Open");
+        MenuItem fileSave = new MenuItem("Save");
+        MenuItem fileSaveAs = new MenuItem("Save As");
+        MenuItem fileExit = new MenuItem("Exit");
+
+        fileMenu.getItems().add(fileNew);
+        fileMenu.getItems().add(fileOpen);
+        fileMenu.getItems().add(fileSave);
+        fileMenu.getItems().add(fileSaveAs);
+        fileMenu.getItems().add(fileExit);
 
         // create a MenuBar instance and add File menu
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().add(fileMenu);
 
-        // get student records table view
-        TableView<StudentRecord> tvStudentRecords = getStudentRecords();
+        ///TableView<StudentRecord> tvStudentRecords = load();
         // use BorderPane as the root layout
         BorderPane root = new BorderPane();
         // add TableView to the root layout
@@ -49,52 +65,108 @@ public class HelloApplication extends Application {
         stage.setTitle("Lab 08 Solution C");
         stage.setScene(scene);
         stage.show();
+
+        // EVENT HANDLERS
+        // when "New" is clicked, clear all items in the TableView
+        fileNew.setOnAction(event -> {
+            tvStudentRecords.getItems().clear();
+        });
+
+        // when "Open" is clicked, load and display csv
+        fileOpen.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // show a file chooser
+                // update the current filename to the user selected filename
+                currentFilename = getCurrentFilename(stage);
+
+                // call load()
+                try {
+                    TableView<StudentRecord> newStudentRecords = load();
+                    root.setCenter(newStudentRecords);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (CsvValidationException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // when "Save" is clicked save the current table view
+        fileSave.setOnAction(event -> {
+            save((TableView<StudentRecord>) root.getCenter());
+        });
+
+        // when "Save As" is clicked, open file chooser and save the current table view
+        fileSaveAs.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // show a file chooser
+                // update the current filename to the user selected filename
+                FileChooser fileChooser = new FileChooser();
+                currentFilename = fileChooser.showSaveDialog(stage).getName();
+
+                // call load()
+                save((TableView<StudentRecord>) root.getCenter());
+            }
+        });
+
+        // when "Exit" is clicked, exit the application
+        fileExit.setOnAction(event -> {
+            stage.close();
+        });
     }
 
-    public static TableView<StudentRecord> getStudentRecords() {
-        // get student marks
-        ObservableList<StudentRecord> marks = getAllMarks();
+    // returns a tableview using data from the marks list
+    public static TableView<StudentRecord> getStudentRecords(ObservableList<StudentRecord> marks) {
+//        // get student marks
+//        ObservableList<StudentRecord> marks = getAllMarks();
 
         // create a TableView object
-        TableView<StudentRecord> tvStudentRecords;
-        tvStudentRecords = new TableView<StudentRecord>(marks);
+        TableView<StudentRecord> studentRecords;
+        studentRecords = new TableView<StudentRecord>(marks);
 
         // associate the columns with column headings
         // use cell factory to populate cells with data
         TableColumn<StudentRecord, String> studentIDColumn = new TableColumn<>("SID");
         studentIDColumn.setCellValueFactory(new PropertyValueFactory<>("studentID"));    // use the data name from data model
-        tvStudentRecords.getColumns().add(studentIDColumn);                             // add column to table
+        studentRecords.getColumns().add(studentIDColumn);                             // add column to table
 
         TableColumn<StudentRecord, String> assignmentsColumn = new TableColumn<>("Assignments");
         assignmentsColumn.setCellValueFactory(new PropertyValueFactory<>("assignmentsGrade"));    // use the data name from data model
-        tvStudentRecords.getColumns().add(assignmentsColumn);                             // add column to table
+        studentRecords.getColumns().add(assignmentsColumn);                             // add column to table
 
         TableColumn<StudentRecord, String> midtermCol = new TableColumn<>("Midterm");
         midtermCol.setCellValueFactory(new PropertyValueFactory<>("midtermGrade"));    // use the data name from data model
-        tvStudentRecords.getColumns().add(midtermCol);                             // add column to table
+        studentRecords.getColumns().add(midtermCol);                             // add column to table
 
         TableColumn<StudentRecord, String> examCol = new TableColumn<>("Final Exam");
         examCol.setCellValueFactory(new PropertyValueFactory<>("examGrade"));    // use the data name from data model
-        tvStudentRecords.getColumns().add(examCol);                             // add column to table
+        studentRecords.getColumns().add(examCol);                             // add column to table
 
         TableColumn<StudentRecord, String> finalCol = new TableColumn<>("Final Mark");
         finalCol.setCellValueFactory(new PropertyValueFactory<>("finalGrade"));    // use the data name from data model
-        tvStudentRecords.getColumns().add(finalCol);                             // add column to table
+        studentRecords.getColumns().add(finalCol);                             // add column to table
 
         TableColumn<StudentRecord, String> letterCol = new TableColumn<>("Midterm");
         letterCol.setCellValueFactory(new PropertyValueFactory<>("letterGrade"));    // use the data name from data model
-        tvStudentRecords.getColumns().add(letterCol);                             // add column to table
+        studentRecords.getColumns().add(letterCol);                             // add column to table
 
-        TableView.TableViewSelectionModel<StudentRecord> tvSelectRecord = tvStudentRecords.getSelectionModel();
+        TableView.TableViewSelectionModel<StudentRecord> tvSelectRecord = studentRecords.getSelectionModel();
 
-        return tvStudentRecords;
+        return studentRecords;
     }
 
-    // pull all data from the TableView and save it to the current filenmae as a CSV
+    // pull all data from the TableView and save it to the current filename as a CSV
     // save only SID, Assignments, Midterms, Final Exam
-    public static void save() {
-        // read data from tableview
-        TableView<StudentRecord> tvStudentRecords = getStudentRecords();
+    public static void save(TableView<StudentRecord> currentTableView) {
+        // get student marks
+        // TODO: this should read from the current TableView
+//        ObservableList<StudentRecord> marks = getAllMarks();
+//
+//        // read data from tableview
+//        TableView<StudentRecord> tvStudentRecords = getStudentRecords(marks);
 
         // create new file
         File thisFile = new File(currentFilename);
@@ -105,8 +177,8 @@ public class HelloApplication extends Application {
             CSVWriter csvWriter = new CSVWriter(new FileWriter(thisFile));
 
             // traverse the student data in the table
-            for (int i = 0; i < tvStudentRecords.getItems().size(); i++) {
-                StudentRecord thisStudent = tvStudentRecords.getItems().get(i);
+            for (int i = 0; i < currentTableView.getItems().size(); i++) {
+                StudentRecord thisStudent = currentTableView.getItems().get(i);
 
                 // get this line of student data
                 String[] thisStudentRec = thisStudent.toString().split(",");
@@ -128,9 +200,40 @@ public class HelloApplication extends Application {
 
     // load the CSV data (as above) from the current filename,
     // and populate the TableView
-    public static void load(TableView<StudentRecord> tvStudentRecords) {
-            
+    public static TableView<StudentRecord> load() throws IOException, CsvValidationException {
+        // new CSV readers instance
+        CSVReader csvReader = new CSVReader(new FileReader(currentFilename));
+        // CSVReader csvReader = new CSVReader(new FileReader("test.csv")); // FOR DEBUGGING PURPOSES
+        String[] nextRecord;
+
+        // DATA SOURCE
+        ObservableList<StudentRecord> marks = FXCollections.observableArrayList();
+
+        // Student ID, Assignments, Midterm, Final exam
+        // read the csv line by line
+        while((nextRecord = csvReader.readNext()) != null) {
+            marks.add(new StudentRecord(nextRecord[0], Float.parseFloat(nextRecord[1]), Float.parseFloat(nextRecord[2]), Float.parseFloat(nextRecord[3])));
+        }
+
+        // FOR DEBUGGING PURPOSES TO CHECK IF DATA WAS READ CORRECTLY
+//        for (StudentRecord mark : marks) {
+//            System.out.println(mark.toString());
+//        }
+
+        return getStudentRecords(marks);
     }
+
+    // open file chooser and get selected filename
+    // returns the file name
+    public static String getCurrentFilename(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        return selectedFile.getName();
+    }
+
+
     public static void main(String[] args) {
         launch();
     }
