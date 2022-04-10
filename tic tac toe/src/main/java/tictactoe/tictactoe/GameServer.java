9034fada044e85e21118cc666a7fcc2035f9bc8e
Chi-O <chioma.o3762@gmail.com>
2022-04-10 12:57:15 -0400

    added ClientHandler (thread) instructions to GameServer

--- tic tac toe/src/main/java/tictactoe/tictactoe/GameServer.java
+++ tic tac toe/src/main/java/tictactoe/tictactoe/GameServer.java
@@ -1,5 +1,7 @@
 package tictactoe.tictactoe;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,6 +10,9 @@ import java.net.Socket;
 public class GameServer {
     private ServerSocket serverSocket = null;
     private int numPlayers;
+
+    private ClientHandler player1;
+    private ClientHandler player2;
 
     // constructor to make new serverSocket at the specified port
     public GameServer() {
@@ -31,11 +36,57 @@ public class GameServer {
                 Socket socket = serverSocket.accept();
                 numPlayers++;
                 System.out.println("Player #" + numPlayers + " connected");
+
+                // use a thread to handle each connection
+                ClientHandler clientHandler = new ClientHandler(socket, numPlayers);
+                if (numPlayers == 1) {
+                    player1 = clientHandler;
+                } else {
+                    player2 = clientHandler;
+                }
+                Thread t = new Thread(clientHandler);
+                t.start();
             }
             System.out.println("Max number of players connected. No longer accepting requests");
         } catch (IOException e) {
             System.out.println("GameServer; acceptConnections()");
             e.printStackTrace();
+        }
+    }
+
+    // use threads to concurrently let two players play
+    // use ClientHandler class to add instructions to run on each thread
+    private class ClientHandler implements Runnable{
+        // the client socket
+        private Socket clientSocket = null;
+        // input and output streams
+        private DataInputStream dis = null;
+        private DataOutputStream dos = null;
+        private int playerID;
+
+        // CONSTRUCTOR: receive the client socket, get input and output stream, assign the playerID
+        public ClientHandler(Socket clientSocket, int playerID) {
+            this.clientSocket = clientSocket;
+            this.playerID = playerID;
+
+            try {
+                dis = new DataInputStream(clientSocket.getInputStream());
+                dos = new DataOutputStream(clientSocket.getOutputStream());
+            } catch (IOException e) {
+                System.out.println("Exception from ClientHandler constructor");
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            // send the player ID
+            try {
+                dos.writeInt(playerID);
+            } catch (IOException e) {
+                System.out.println("Exception from ClientHandler run()");
+                e.printStackTrace();
+            }
         }
     }
 
